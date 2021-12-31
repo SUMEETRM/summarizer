e8f8a4284ee9af24ec27f51476426b5d720b297e
Glitch (summarizer) <none>
2021-12-31 11:53:44 +0000

    🌓🍵 Updated with Glitch

---
+++ sketch.js
@@ -0,0 +1,36 @@
+console.log(chrome);
+console.log(chrome.extension);
+console.log(chrome.extension.getBackgroundPage());
+console.log(chrome.extension.getBackgroundPage().word);
+
+let bgpage = chrome.extension.getBackgroundPage();
+let word = bgpage.word;
+//createP(word);
+
+const data = JSON.stringify({
+  text: word,
+  num_sentences: 2
+});
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function() {
+  if (this.readyState === this.DONE) {
+    console.log(this.responseText);
+    var summary = this.responseText;
+    document.getElementById("summary").innerHTML=summary;
+  }
+});
+
+document.write(word);
+
+xhr.open("POST", "https://gpt-summarization.p.rapidapi.com/summarize");
+xhr.setRequestHeader("content-type", "application/json");
+xhr.setRequestHeader(
+  "x-rapidapi-key",
+  "e9bb7a8714mshb718c77ffb4594cp1712b3jsn315e431a60cd"
+);
+xhr.setRequestHeader("x-rapidapi-host", "gpt-summarization.p.rapidapi.com");
+
+xhr.send(data);
